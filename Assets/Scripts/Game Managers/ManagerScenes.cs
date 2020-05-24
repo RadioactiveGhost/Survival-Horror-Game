b64041129d84fceb0e5bde3b05018acec49c9a83
Gatinha123 <andreia.fer.mendes@gmail.com>
2020-05-24 16:07:33 +0100

    MainMenu added, scene + menu navigation, sound manager and player preferences

---
+++ Assets/Scripts/Game Managers/ManagerScenes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerScenes : MonoBehaviour
+{
+    public enum scenes //NAME SENSITIVE
+    {
+        MainMenu = 0,
+        Game = 1,
+        Lose = 2
+    };
+
+    public static void LoadScene(scenes s)//Experimental untested
+    {
+        Debug.Log("Loading " + s.ToString());
+        SceneManager.LoadScene(s.ToString());
+    }
+
+    public static void LoadMainMenu()
+    {
+        LoadScene(scenes.MainMenu);
+    }
+
+    public void bcsUnityLoadMainMenu() //buttons don't take static functions -.-
+    {
+        LoadMainMenu();
+    }
+
+    public static void LoadGame()
+    {
+        LoadScene(scenes.Game);
+    }
+
+    public void bcsUnityLoadgame() //buttons don't take static functions -.-
+    {
+        LoadGame();
+    }
+
+    public static void Quit()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+
+    public void bcsUnityQuit() //buttons don't take static functions -.-
+    {
+        Quit();
+    }
+}
