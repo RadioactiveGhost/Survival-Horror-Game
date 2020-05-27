009b6b3a215973e33dbd5b4766d6259bc99a3df5
flameoncat <francisco.morim@gmail.com>
2020-05-27 11:29:33 +0100

    ja funciona - erro encontrado...
    sistema de familias e packs precisa ser remastered

--- Assets/Scripts/AnimalSpawner.cs
+++ Assets/Scripts/AnimalSpawner.cs
@@ -13,40 +13,40 @@ public class AnimalSpawner : MonoBehaviour
     Vector3 rngposition;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     { 
 
         terrainScript = terrain.GetComponent<TerrainGenerator>();
+        if (wolves)
+            Spawn((GameObject)Resources.Load<GameObject>("Animals/wolf"), (int)Random.Range(minNumber, maxNumber));
+        if (bears)
+            Spawn((GameObject)Resources.Load<GameObject>("Animals/bear"), (int)Random.Range(minNumber, maxNumber));
+        if (boars)
+            Spawn((GameObject)Resources.Load<GameObject>("Animals/boar"), (int)Random.Range(minNumber, maxNumber));
+        if (deers)
+            Spawn((GameObject)Resources.Load<GameObject>("Animals/deer"), (int)Random.Range(minNumber, maxNumber));
+        if (bunnies)
+            Spawn((GameObject)Resources.Load<GameObject>("Animals/bunny"), (int)Random.Range(minNumber, maxNumber));
+        if (boarpacks)
+            Spawn((GameObject)Resources.Load<GameObject>("Animals/Boars/boarPack1"), (int)Random.Range(minNumber, maxNumber));
+        if (wolfpacks)
+            Spawn((GameObject)Resources.Load<GameObject>("Animals/Wolves/wolfpack1"), (int)Random.Range(minNumber, maxNumber));
     }
 
-    void Spawn(GameObject animal, int number, bool animalFlag)
+    void Spawn(GameObject animal, int number)
     {
-        for (int i = 0; i == number; i++)
+        for (int i = 0; i < number; i++)
         {
             Debug.Log("SPAWNED YEY");
-            GameObject newAnimal = Instantiate(animal, new Vector3(Random.Range(0, terrainScript.sizeXtile * terrainScript.mapSizeX), 11f,
+            GameObject newAnimal = Instantiate(animal, new Vector3(Random.Range(0, terrainScript.sizeXtile * terrainScript.mapSizeX), 1.5f,
                   Random.Range(0, terrainScript.sizeZtile * terrainScript.mapSizeY)), Quaternion.identity);
         }
-        animalFlag = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (wolves)
-            Spawn((GameObject)Resources.Load<GameObject>("Animals/wolf"), (int)Random.Range(minNumber, maxNumber), wolves);
-        if (bears)
-            Spawn((GameObject)Resources.Load<GameObject>("Animals/bear"), (int)Random.Range(minNumber, maxNumber), bears);
-        if (boars)
-            Spawn((GameObject)Resources.Load<GameObject>("Animals/boar"), (int)Random.Range(minNumber, maxNumber), boars);
-        if (deers)
-            Spawn((GameObject)Resources.Load<GameObject>("Animals/deer"), (int)Random.Range(minNumber, maxNumber), deers);
-        if (bunnies)
-            Spawn((GameObject)Resources.Load<GameObject>("Animals/bunny"), (int)Random.Range(minNumber, maxNumber), bunnies);
-        if (boarpacks)
-            Spawn((GameObject)Resources.Load<GameObject>("Animals/Boars/boarPack1"), (int)Random.Range(minNumber, maxNumber), boarpacks);
-        if (wolfpacks)
-            Spawn((GameObject)Resources.Load<GameObject>("Animals/Wolves/wolfpack1"), (int)Random.Range(minNumber, maxNumber), wolfpacks);
+        
 
     }
 }
