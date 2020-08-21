e5d7228fa80ee6f840479b1670f5c974bbe13481
Gatinha123 <andreia.fer.mendes@gmail.com>
2020-08-21 10:59:09 +0100

    MainMenu
    Buttons added and functioneal, pause/options menu added to aminMenu

--- Assets/Scripts/Game Managers/UserSettings.cs
+++ Assets/Scripts/Game Managers/UserSettings.cs
@@ -3,4 +3,10 @@
 public class UserSettings : MonoBehaviour
 {
     public static float volume = 1;
+
+    public void SetNewVolume(float volume)
+    {
+        UserSettings.volume = volume;
+        SoundManager.UpdateVolume();
+    }
 }
