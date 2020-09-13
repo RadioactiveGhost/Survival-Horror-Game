eca0f2df8a4b40b70def53e37ef3f64eed1e6356
Gatinha123 <andreia.fer.mendes@gmail.com>
2020-09-13 19:01:58 +0100

    Cave soundtrack revamp + fade in + out

---
+++ Assets/Scripts/Resources/Common.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Common
+{
+    //helpers
+    static int dmg;
+
+    public static int TakeDamage(int damage, int health, int armor)
+    {
+        dmg = (damage / 2) - armor;
+        if (dmg < 0)
+        {
+            dmg = 0;
+        }
+        damage = (damage / 2) + dmg;
+
+        return TakeTrueDamage(damage, health);
+    }
+
+    public static int TakeTrueDamage(int damage, int health)
+    {
+        health -= damage;
+        if (health < 0)
+        {
+            health = 0;
+        }
+
+        return health;
+    }
+
+    public static int Heal(int health, int amount)
+    {
+        health += amount;
+        return health;
+    }
+
+    public static int Attack(int strength, float modifier)
+    {
+        if (modifier >= 1)
+        {
+            Debug.LogWarning("Consider balancing dmg, dmg is done: dmg = srength + strength * modifier, current modifier should not go past 1, so it's at max double dmg");
+        }
+        return (int)(strength + strength * modifier);
+    }
+}
--- Assets/Scripts/Sound/CaveSoundManager.cs
+++ Assets/Scripts/Sound/CaveSoundManager.cs
@@ -3,22 +3,87 @@ using UnityEngine;
 
 public class CaveSoundManager : MonoBehaviour
 {
-    IEnumerator DelayPLayMusic()
+    Player p;
+    string s, temp;
+    public int duration;
+
+    IEnumerator DelayPLayMusic(string s, int delayInSeconds)
     {
-        SoundManager.StopAll();
+        yield return new WaitForSeconds(delayInSeconds);
 
-        yield return new WaitForSeconds(5);
-
-        SoundManager.Play("Cave soundtrack");
+        SoundManager.Play(s);
     }
 
     private void Start()
     {
-        StartCoroutine(DelayPLayMusic());
+        p = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        SoundManager.Play("Cave soundtrack");
+        SoundManager.SetVolume("Cave soundtrack", 1);
+        SoundManager.Play("Cave soundtrack 2");
+        SoundManager.SetVolume("Cave soundtrack 2", 0);
+        SoundManager.Play("Cave soundtrack 3");
+        SoundManager.SetVolume("Cave soundtrack 3", 0);
+
+        temp = ChooseSoundtrack();
+        ChangeSoundtrackVolumes();
+
+        s = temp;
     }
 
-    void Update()
+    private void Update()
     {
-        
+        temp = ChooseSoundtrack();
+        if (s != temp)
+        {
+            ChangeSoundtrackVolumes();
+        }
+        s = temp;
+    }
+
+    private string ChooseSoundtrack()
+    {
+        if(p.health >= p.maxHealth * 0.8)
+        {
+            return "Cave soundtrack";
+        }
+        else if(p.health >= p.maxHealth * 0.4)
+        {
+            return "Cave soundtrack 2";
+        }
+        else
+        {
+            return "Cave soundtrack 3";
+        }
+    }
+
+    void ChangeSoundtrackVolumes()
+    {
+        //fade in
+        if(temp == "Cave soundtrack")
+        {
+            SoundManager.FadeInOut(duration, temp, true, false);
+        }
+        else if(temp == "Cave soundtrack 2")
+        {
+            SoundManager.FadeInOut(duration, temp, true, false);
+        }
+        else if (temp == "Cave soundtrack 3")
+        {
+            SoundManager.FadeInOut(duration, temp, true, false);
+        }
+
+        //fade out
+        if (s == "Cave soundtrack")
+        {
+            SoundManager.FadeInOut(duration, s, false, false);
+        }
+        else if (s == "Cave soundtrack 2")
+        {
+            SoundManager.FadeInOut(duration, s, false, false);
+        }
+        else if(s == "Cave soundtrack 3")
+        {
+            SoundManager.FadeInOut(duration, s, false, false);
+        }
     }
 }
